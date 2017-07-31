2c8e92d8ee4bb456f9a78ad4977eff570ac294c9
Ketan <ketanjawahire2012@gmail.com>
2017-07-31 18:41:09 +0530

    Added new exception type - SDKException. This will be thrown for all SDK related errors.

---
+++ FacebookApi/FacebookApi/Exceptions/SDKException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookApi.Exceptions
+{
+    /// <summary>
+    /// Represents error in SDK while deserializing api responce
+    /// </summary>
+    public class SDKException : Exception
+    {
+        /// <summary>
+        /// Content of api responce as string
+        /// </summary>
+        public string ApiResponseContent { get; private set; }
+
+        /// <summary>
+        /// Initialize new instance of <see cref="SDKException"/>
+        /// </summary>
+        /// <param name="apiResponseContent">Content of api responce as string</param>
+        /// <param name="ex">Exception</param>
+        public SDKException(string apiResponseContent, Exception ex) : base(ex.Message, ex)
+        {
+            ApiResponseContent = apiResponseContent;
+        }
+    }
+}
