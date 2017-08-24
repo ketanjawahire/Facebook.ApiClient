c07e893f5d45d14cfab743431e09d2f9e162e004
Ketan <ketanjawahire2012@gmail.com>
2017-08-24 11:05:50 +0530

    corrected entites namespaces

---
+++ FacebookApi/FacebookApi/Entities/ApiEngine/BatchRequestBodyParameter.cs
@@ -0,0 +1,20 @@
+﻿using FacebookApi.Interfaces.IApiEngine;
+
+namespace FacebookApi.Entities.ApiEngine
+{
+    /// <summary>
+    /// Represents batch request body parameter
+    /// </summary>
+    public class BatchRequestBodyParameter : IBatchRequestBodyParameter
+    {
+        /// <summary>
+        /// Name of the parameter
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Value of the parameter
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ FacebookApi/FacebookApi/Entities/ApiEngine/BatchRequestParameters.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using FacebookApi.Enumerations.ApiEngine;
+using FacebookApi.Interfaces.IApiEngine;
+
+namespace FacebookApi.Entities.ApiEngine
+{
+    /// <summary>
+    /// Represents Batch request parameter
+    /// </summary>
+    public class BatchRequestParameters : IBatchRequestParameters
+    {
+        /// <summary>
+        /// Name of the request. Used to identify request within batch.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Request method
+        /// </summary>
+        public ApiRequestHttpMethod Method { get; set; }
+
+        /// <summary>
+        /// Request url
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Collection of request body parameters
+        /// </summary>
+        public IList<BatchRequestBodyParameter> Body { get; set; }
+
+        /// <summary>
+        /// If its a dependent request then specify dependent request
+        /// </summary>
+        public BatchRequestParameters DependsOn { get; set; }
+
+        /// <summary>
+        /// Initialize new instance of <see cref="BatchRequestParameters"/>
+        /// </summary>
+        public BatchRequestParameters()
+        {
+            Body = new List<BatchRequestBodyParameter>();
+        }
+    }
+}
