b2ca40eb90c8193ce5c5e5400c1b044b51e141b1
Ketan <ketanjawahire2012@gmail.com>
2017-12-13 15:36:49 +0530

    Update Program.cs
    Removed unnecessary using blocks

--- SampleConsoleApplication/SampleConsoleApplication/Program.cs
+++ SampleConsoleApplication/SampleConsoleApplication/Program.cs
@@ -1,17 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FacebookApi.ApiEngine;
-using FacebookApi.Entities;
-using FacebookApi.Entities.Api;
-using FacebookApi.Enumerations.ApiEngine;
-using FacebookApi.Interfaces;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using RestSharp.Newtonsoft.Json;
 
 namespace SampleConsoleApplication
 {
